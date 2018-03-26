37fc9f1edf5f4049a7fe72bf30a06aa2a646428e
Thomas Damiani <thomasdamiani13@gmail.com>
2018-03-26 17:05:37 +0200

    ajout filter

--- challenge/pipeline.c
+++ challenge/pipeline.c
@@ -7,9 +7,48 @@
 #include <fft.h>
 
 #define PI 3.14159265359
+#define SIZE_NEIGHBOURHOOD 3
 
 float deg2rad(float deg) {
     return deg * PI / 180.0;
+}
+
+float get_mean_neighbourhood(pnm img, int i, int j){
+    int cols = pnm_get_width(img);
+    int rows = pnm_get_height(img);
+
+    int nb_neighbours = 0;
+    float sum = 0;
+    for(int x = i - SIZE_NEIGHBOURHOOD / 2; x < i + SIZE_NEIGHBOURHOOD / 2; x++){
+        for(int y = j - SIZE_NEIGHBOURHOOD / 2; y < j + SIZE_NEIGHBOURHOOD / 2; y++){
+            if(x >= 0 && x < cols &&
+               y >= 0 && y < rows){
+                   unsigned short value = pnm_get_component(img, y, x, PnmRed);
+                   nb_neighbours++;
+                   sum += value;
+            }
+        }
+    }
+
+    return sum / nb_neighbours;
+}
+
+pnm filter(pnm ims){
+    int cols = pnm_get_width(ims);
+    int rows = pnm_get_height(ims);
+
+    pnm res = pnm_new(cols, rows, PnmRawPpm);
+
+    for(int i = 0; i < cols; i++){
+        for(int j = 0; j < rows; j++){
+            float mean = get_mean_neighbourhood(ims, i, j);
+            for(int c = 0; c < 3; c++){
+                pnm_set_component(res, j, i, c, mean);
+            }
+        }
+    }
+
+    return res;
 }
 
 void rotate(int x_center, int y_center, float angle, pnm ims, pnm imd){
@@ -37,12 +76,13 @@ void process(char *ims_name, char *imd_name){
     // pnm imd = pnm_new(imd_name, cols, rows, PnmRawPpm);
     pnm imd;
     imd = filter(ims);
-    pnm_free(ims);
+    pnm_save(imd, PnmRawPpm, imd_name);
+    /*pnm_free(ims);
     ims = imd;
     imd = thresholding(100, ims);
     pnm_save(imd_name);
     pnm_free(ims);
-    pnm_free(imd);
+    pnm_free(imd);*/
 
 }
 
