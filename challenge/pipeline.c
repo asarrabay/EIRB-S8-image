6b6db15d110eae8eab4e6469311f1a816f1367a0
Alexis SARRABAYROUSE <alexis.sarrabayrouse@gmail.com>
2018-03-26 17:08:20 +0200

    thresholding

--- challenge/pipeline.c
+++ challenge/pipeline.c
@@ -8,9 +8,38 @@
 
 #define PI 3.14159265359
 
+struct coord {
+    int x;
+    int y;
+};
+
+struct coord find_circle(pnm img){
+    (void) img;
+    struct coord c;
+    c.x = 0;
+    c.y = 0;
+    return c;
+}
+
 float deg2rad(float deg) {
     return deg * PI / 180.0;
 }
+
+pnm thresholding(unsigned short threshold, pnm ims){
+    int rows = pnm_get_height(ims);
+    int cols = pnm_get_width(ims);
+    pnm imd = pnm_dup(ims);
+    unsigned short *data_imd = pnm_get_image(imd);
+    for (int i = 0; i < cols * rows * 3; i++) {
+        if (data_imd[i] < threshold) {
+            data_imd[i] = 0;
+        } else {
+            data_imd[i] = 255;
+        }
+    }
+    return imd;
+}
+
 
 void rotate(int x_center, int y_center, float angle, pnm ims, pnm imd){
     int rows = pnm_get_height(ims);
@@ -32,15 +61,17 @@ void rotate(int x_center, int y_center, float angle, pnm ims, pnm imd){
 
 void process(char *ims_name, char *imd_name){
     pnm ims = pnm_load(ims_name);
-    int cols = pnm_get_width(ims);
-    int rows = pnm_get_height(ims);
+    // int cols = pnm_get_width(ims);
+    // int rows = pnm_get_height(ims);
     // pnm imd = pnm_new(imd_name, cols, rows, PnmRawPpm);
     pnm imd;
-    imd = filter(ims);
-    pnm_free(ims);
-    ims = imd;
-    imd = thresholding(100, ims);
-    pnm_save(imd_name);
+    // imd = filter(ims);
+    // pnm_free(ims);
+    // ims = imd;
+    imd = thresholding(200, ims);
+    struct coord circle = find_circle(imd);
+    (void) circle;
+    pnm_save(imd, PnmRawPpm, imd_name);
     pnm_free(ims);
     pnm_free(imd);
 
