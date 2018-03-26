a5c1a2e4eff6f352b0b9bc5b6924178b87a85199
Alexis SARRABAYROUSE <alexis.sarrabayrouse@gmail.com>
2018-03-26 16:08:04 +0200

    Challenge

--- challenge/pipeline.c
+++ challenge/pipeline.c
@@ -2,13 +2,48 @@
 #include <stdio.h>
 #include <pnm.h>
 #include <math.h>
+#include <domain.h>
 
 #include <fft.h>
 
+#define PI 3.14159265359
+
+float deg2rad(float deg) {
+    return deg * PI / 180.0;
+}
+
+void rotate(int x_center, int y_center, float angle, pnm ims, pnm imd){
+    int rows = pnm_get_height(ims);
+    int cols = pnm_get_width(ims);
+    float cos_t = cosf(deg2rad(angle));
+    float sin_t = sinf(deg2rad(angle));
+
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            float x = cos_t * (j-x_center) - sin_t * (i-y_center) + x_center;
+            float y = sin_t * (j-x_center) + cos_t * (i-y_center) + y_center;
+            for (int c = 0; c < 3; c++) {
+                pnm_set_component(imd, i, j, c, bilinear_interpolation(x, y, ims, c));
+            }
+        }
+    }
+}
 
 void process(char *ims_name, char *imd_name){
-    (void) ims_name;
-    (void) imd_name;
+    pnm ims = pnm_load(ims_name);
+    int cols = pnm_get_width(ims);
+    int rows = pnm_get_height(ims);
+    // pnm imd = pnm_new(imd_name, cols, rows, PnmRawPpm);
+    pnm imd;
+    imd = filter(ims);
+    pnm_free(ims);
+    ims = imd;
+    imd = thresholding(100, ims);
+    pnm_save(imd_name);
+    pnm_free(ims);
+    pnm_free(imd);
+
 }
 
 void usage(char* s) {
